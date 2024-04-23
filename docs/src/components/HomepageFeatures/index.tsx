0c9ee68bd77846debd45a81892e6741cacc80a71
Adrian Schubek <adrian.schubek@gmail.com>
2024-04-23 19:56:33 +0200

    docs: update homepage

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -13,7 +13,10 @@ export default function HomepageFeatures(): JSX.Element {
               <img style={{ maxHeight: "100px" }} src="/img/docker.webp"></img>
             </div>
             <div className="text--center padding-horiz--md">
-              <h3>Easy Docker deployment<Checkmark/></h3>
+              <h3>
+                Easy Docker deployment
+                <Checkmark />
+              </h3>
               <p>
                 Application is self-contained in a single image and ready to be
                 deployed.
@@ -25,7 +28,10 @@ export default function HomepageFeatures(): JSX.Element {
               <img style={{ maxHeight: "100px" }} src="/img/counter.svg"></img>
             </div>
             <div className="text--center padding-horiz--md">
-              <h3>Download Counter<Checkmark/></h3>
+              <h3>
+                Download Counter
+                <Checkmark />
+              </h3>
               <p>
                 Tracks the number of times a file has been downloaded and stores
                 it in a <i>Redis</i> database.
@@ -34,12 +40,15 @@ export default function HomepageFeatures(): JSX.Element {
           </div>
           <div className={clsx("col col--4")}>
             <div className="text--center">
-              <img style={{ maxHeight: "100px" }} src="/img/secure.svg"></img>
+              <img style={{ maxHeight: "100px" }} src="/img/metadata.png"></img>
             </div>
             <div className="text--center padding-horiz--md">
-              <h3>Secure by default<Checkmark/></h3>
+              <h3>
+                Custom description & labels
+                <Checkmark />
+              </h3>
               <p>
-                Strict <b>Read-only</b> access to files and folders.
+                Display additional information about your files & folders with custom metadata.
               </p>
             </div>
           </div>
@@ -50,30 +59,45 @@ export default function HomepageFeatures(): JSX.Element {
               <img style={{ maxHeight: "100px" }} src="/img/readme.svg"></img>
             </div>
             <div className="text--center padding-horiz--md">
-              <h3>README Markdown Rendering<Checkmark/></h3>
+              <h3>
+                README Markdown Rendering
+                <Checkmark />
+              </h3>
               <p>Automatically renders Markdown README files.</p>
             </div>
-          </div>          
+          </div>
           <div className={clsx("col col--4")}>
             <div className="text--center">
               <img style={{ maxHeight: "100px" }} src="/img/colorp.svg"></img>
             </div>
             <div className="text--center padding-horiz--md">
-              <h3>Themes & Darkmode<Checkmark/></h3>
+              <h3>
+                Themes & Darkmode
+                <Checkmark />
+              </h3>
               <p>
-                Choose from many different <b>themes</b> with <b>darkmode</b> support.
+                Choose from many different <b>themes</b> with <b>darkmode</b>{" "}
+                support.
               </p>
             </div>
           </div>
           <div className={clsx("col col--4")}>
             <div className="text--center">
               <img
-                style={{ maxHeight: "100px", maxWidth: "150px", paddingBottom: "2em", paddingTop: "2em" }}
+                style={{
+                  maxHeight: "100px",
+                  maxWidth: "150px",
+                  paddingBottom: "2em",
+                  paddingTop: "2em",
+                }}
                 src="/img/nginx.svg"
               ></img>
             </div>
             <div className="text--center padding-horiz--md">
-              <h3>Fast file serving<Checkmark/></h3>
+              <h3>
+                Fast file serving
+                <Checkmark />
+              </h3>
               <p>
                 NGINX, a high performance reverse proxy, handles all file
                 serving to maximize performance.
@@ -84,12 +108,15 @@ export default function HomepageFeatures(): JSX.Element {
         <div className="row">
           <div className={clsx("col col--4")}>
             <div className="text--center">
-              <img style={{ maxHeight: "100px" }} src="/img/auth.svg"></img>
+              <img style={{ maxHeight: "100px" }} src="/img/secure.svg"></img>
             </div>
             <div className="text--center padding-horiz--md">
-              <h3>Password Protection<Checkmark/></h3>
+              <h3>
+                Secure by default
+                <Checkmark />
+              </h3>
               <p>
-                Want to share files with a small group of people? Simply protect everything with a password.
+                Strict <b>Read-only</b> access to files and folders.
               </p>
             </div>
           </div>
@@ -98,7 +125,10 @@ export default function HomepageFeatures(): JSX.Element {
               <img style={{ maxHeight: "100px" }} src="/img/ignore.svg"></img>
             </div>
             <div className="text--center padding-horiz--md">
-              <h3>Hide files & folders<Checkmark/></h3>
+              <h3>
+                Hide files & folders
+                <Checkmark />
+              </h3>
               <p>
                 Want to hide your dot files? Hide old documents? You can do it.
               </p>
@@ -109,7 +139,10 @@ export default function HomepageFeatures(): JSX.Element {
               <img style={{ maxHeight: "100px" }} src="/img/settings.svg"></img>
             </div>
             <div className="text--center padding-horiz--md">
-              <h3>Highly configurable<Checkmark/></h3>
+              <h3>
+                Highly configurable
+                <Checkmark />
+              </h3>
               <p>
                 <b>Customize</b> the application to your needs by using
                 environment variables.
